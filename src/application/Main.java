d5c8cf787eadba1b55d56eb34b755d0342305eb0
JackRyan <chaostype@gmx.de>
2016-11-09 21:35:24 +0100

    pups

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,6 +3,7 @@ package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 
 
@@ -12,6 +13,9 @@ public class Main extends Application {
 		try {
 			BorderPane root = new BorderPane();
 			Scene scene = new Scene(root,400,400);
+			Label label = new Label("Ich musss pupsen!!");
+			root.getChildren().add(label);
+			
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
