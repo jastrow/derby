261745ba1d6f3c7335c8bb7ca18dc35220f6a9ca
JackRyan <chaostype@gmx.de>
2016-11-12 11:55:41 +0100

    no message

--- src/application/ctrl_Spiel.java
+++ src/application/ctrl_Spiel.java
@@ -1,5 +1,18 @@
 package application;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class ctrl_Spiel {
+
+    @FXML
+    private Button Spieler_1;
+
+    @FXML
+    void bewegePferdSpieler1(ActionEvent event) {
+    	System.out.println("Test");
+
+    }
 
 }
