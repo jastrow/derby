457236250f97e5c0878f0ca8d9da5dd2676731e1
MIMISOU\Salim <box@slimou.net>
2016-11-12 09:16:17 +0100

    nur ein Test mit dem CSS
    Signed-off-by: MIMISOU\Salim <box@slimou.net>

--- Derby/src/application/Main.java
+++ Derby/src/application/Main.java
@@ -3,7 +3,9 @@ package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 
 
 public class Main extends Application {
@@ -11,6 +13,11 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			BorderPane root = new BorderPane();
+
+			VBox text = new VBox();
+			text.getChildren().add(new Label("Kentucky Derby"));
+			root.setTop(text);
+
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setTitle("Kentucky Derby");
