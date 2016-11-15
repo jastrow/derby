3c11b90701bd596a90db6c0057fa8c9fc49a0f98
Mike Jastrow <mj@mdnetz.de>
2016-11-15 08:36:30 +0100

    Configurationsklasse, sollten wir überall einbinden wo die zahlen interessant sind (allein schon um besser testen zu können)

---
+++ src/application/Configuration.java
@@ -0,0 +1,13 @@
+package application;
+
+public class Configuration {
+	private static Integer streckenlaenge = 27;
+	private static Integer anzahlBahnen = 12;
+	
+	public static Integer getStreckenlaenge() {
+		return Configuration.streckenlaenge;
+	}
+	public static Integer getAnzahlBahnen() {
+		return Configuration.anzahlBahnen;
+	}
+}
